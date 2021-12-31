e6f906a420c8b6338c4d5a49937a9c86c070e59a
unknown <hakhanhlinh536@gmail.com>
2021-12-31 23:39:28 +0700

    message

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chin chào ck iu của iem ! 🖐🖐🖐',
+    introDesc: `Vk có điều này muốn hỏi Ck, Ck nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Vk biết Ck iu vk nhất phải không phải không ? 😙',
+    desc: 'Ck không trả lời hoặc thoát ra, thì bank vk 500k nha :> ',
+    btnYes: 'Có, Ck rất iu vk 💖💖💖',
+    btnNo: '💔  Mơ à ! 💔 ',
+    question:'Nói cho VK biết đi vì sao ck iu vk thế 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Vk xinh gái lại còn tốt bụng đó Ahiiiii 😊😊',
+    mess: 'vk biết mà, vk cũng thích ck lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho vk nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/messages/t/100020906259462/'
+}
